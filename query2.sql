2a8369cc17924cff0c60f6b088ad259b07e1ac2b
Sneha-4522 <85059879+Sneha-4522@users.noreply.github.com>
2021-06-19 22:39:07 +0530

    Update query2.sql

--- query2.sql
+++ query2.sql
@@ -1,5 +1,6 @@
-select b.title, p.name
-from books as b
-inner join publisher as p
-on p.id = b.publisher
-where p.country = 'UK';
+Queri2. sql
+-- ### All books published by UK publishers
+-- Creating a file called `query2.sql` which will contain a query that
+-- will print the names and publishers of all books by published by
+-- publishers in the UK
+select b.title, p.name from books b, publisher p where b.publisher = p.id and p.country = 'UK';
