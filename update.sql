2836869620e6b0e66a8f2636a6cd69bcad401e54
Sneha-4522 <85059879+Sneha-4522@users.noreply.github.com>
2021-06-19 23:18:41 +0530

    Update update.sql

--- update.sql
+++ update.sql
@@ -1,4 +1,3 @@
-
 -- ## Updating
 -- ### Change PHI to Prentice Hall
 -- Write a file called `update1.sql` which will contain a SQL statement to change the name of the -- publisher PHI to "Prentice Hall"
